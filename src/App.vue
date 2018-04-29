bebca8c5e4c0214b1a9ac0b1b73a53865a5a9da7
Raymond Nazaryan <rayn2427@gmail.com>
2018-04-29 15:36:24 -0700

    Added mixins

--- src/App.vue
+++ src/App.vue
@@ -18,25 +18,18 @@
 
 <script>
     import List from './List.vue';
+    import { fruitMixin } from './fruitMixin'
 
     export default {
+        mixins: [fruitMixin],
         data() {
             return {
                 text: 'Hello there!',
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
             }
         },
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
-            }
-        },
-        computed: {
-            filteredFruits() {
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                });
             }
         },
         components: {
