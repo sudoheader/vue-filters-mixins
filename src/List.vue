6e7864a4439f8b9b0be5f97fe212feef0d8af511
Raymond Nazaryan <rayn2427@gmail.com>
2018-04-30 17:42:16 -0700

    Merged mixins

--- src/List.vue
+++ src/List.vue
@@ -12,7 +12,10 @@
     import { fruitMixin } from './fruitMixin'
 
     export default {
-        mixins: [fruitMixin]
+        mixins: [fruitMixin],
+        created() {
+            console.log('Inside List Created Hook');
+        }
     }
 </script>
 
